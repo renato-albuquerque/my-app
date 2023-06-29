b9ea2fde360a30e513d4f35e9296d8fcf2032c02
Renato Albuquerque <renatomalb83@gmail.com>
2023-06-29 01:28:36 -0300

    updated project

--- src/App.js
+++ src/App.js
@@ -62,6 +62,32 @@ const channelListData = [
 
 ]
 
+const socialListData = [
+  {
+    url: "https://www.instagram.com/renatomalb/", 
+    imageUrl:"/assets/instagram.svg",    
+    alt: "Instagram do Renato Albuquerque",
+  },
+
+  {
+    url: "https://www.facebook.com/renato.albuquerque.501/", 
+    imageUrl:"/assets/facebook.svg",    
+    alt: "Facebook do Renato Albuquerque",
+  },
+
+  {
+    url: "https://github.com/renato-albuquerque/", 
+    imageUrl:"/assets/github.svg",    
+    alt: "Github do Renato Albuquerque",
+  },
+
+  {
+    url: "https://www.linkedin.com/in/renato-malbuquerque/", 
+    imageUrl: "/assets/linkedin.svg",    
+    alt: "Linkedin do Renato Albuquerque",
+  },
+]
+
 function App() {
   return (
     <div className="App">
@@ -112,7 +138,17 @@ function App() {
           className="social-list"
         >  
 
-
+        {
+          socialListData.map(function(item) {
+            return (
+              <ListItem 
+                url={item.url}
+                imageUrl={item.imageUrl}
+                alt={item.alt}
+              />
+            )
+          })
+        }
         </Section>
       
       </main>  
